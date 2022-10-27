793c6afb351794962a96efaf4ec94770ccd5c971
Hong-Phat <hongphatcj.111@gmail.com>
2022-10-27 15:54:42 +0700

    first commit

---
+++ DoAnThucTap/application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,27 @@
+<h1>Trang giới thiệu</h1>
+<div id="page-bodyhtml" class="bodytext margin-bottom-lg">
+    		<h1 style="text-align:center"><span style="font-size:26px;"><strong><span style="color:rgb(41, 128, 185);"><span style="font-family:'Times New Roman',serif">CTY TNHH THƯƠNG MẠI DỊCH VỤ CÔNG NGHỆ HUY MINH <sup>PRO</sup></span></span></strong></span></h1>
+<br>
+<span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><em><span style="color:black"><span style="font-family:Times New Roman,Times,serif;"><strong>T</strong><b>ên công ty viết tắt</b>:</span> </span></em><span style="font-family:'Times New Roman','serif'"><em>Cty TNHH TM DV CÔNG NGHỆ HUY MINH<sup>Pro</sup>.<br>
+<strong>MST</strong>: 1801 617 501</em><br>
+<strong>Địa chỉ trụ sở chính</strong>: 43 đường số 9, phường An Khánh, quận Ninh Kiều, TP Cần Thơ.<br>
+<strong>VP:</strong>61 Long Định, phường Long Hưng, quận Ô Môn, TP Cần Thơ.<br>
+<strong>Điện thoại</strong>: 0939 771 987<br>
+<strong>Email</strong>: ndkhoa202@gmail.com - website: <a href="http://huyminhcantho.com">huyminhcantho.com</a></span></span></span></span>
+
+<div style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'"><span style="color:black">Hình thức: Công ty TNHH TM DV Công Nghệ Huy Minh <sup>Pro</sup> là doanh nghiệp được thành lập theo loại hình công ty trách nhiệm hữu hạn tiền thân là công ty TNHH Tin Học Viễn Thông Bảo Khoa, được tổ chức và hoạt động theo Luật Đầu tư và Luật Doanh nghiệp do Quốc hội nước Cộng hòa xã hội chủ nghĩa Việt Nam khóa XI thông qua ngày 29/11/2005.<br>
+Với bề dầy hơn 09&nbsp;năm (thành lập từ năm 2011) trên thị trường, chúng tôi tự tin sẽ đáp ứng được nhu cầu của khách hàng.</span></span></span></span></span></div>
+
+<div style="margin-left: 40px;"><br>
+<span style="font-weight: bold; font-family: 'VNtimes new roman', sans-serif; font-size: 13pt;"><span style="font-family:'Times New Roman','serif'"><span style="font-size:20px;">C</span><span style="font-size:20px;">ác hoạt động chuyên ngành:</span></span></span></div>
+
+<ul>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'"><span style="color:black">Lập trình phần mềm theo yêu cầu</span></span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'"><span style="color:black">Bán lẻ thiết bị nghe nhìn trong các cửa hàng chuyên doanh</span></span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'">Tư vấn máy vi tính và quản trị hệ thống máy vi tính</span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'">Hoạt động dịch vụ công nghệ thông tin và dịch vụ khác liên quan đến máy vi tính</span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'">Sửa chữa máy vi tính và thiết bị ngoại vi</span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'"><span style="color:black">Lập trình web</span></span></span></span></span></li>
+	<li style="margin-left: 40px;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:13.0pt"><span style="font-family:'Times New Roman','serif'"><span style="color:black">Bán buôn máy vi tính, thiết bị ngoại vi</span></span></span></span></span></li>
+</ul>
+    	</div>
